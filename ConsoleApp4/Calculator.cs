8f758f623ed7996035d095e42f661caa5b4545e5
Danielzhel <fartunazh@gmail.com>
2022-10-20 14:20:54 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Calculator.cs
@@ -0,0 +1,116 @@
+﻿using LIbrary;
+using System;
+
+Console.Title = "Calculator";
+
+double firstValue;
+double secondValue;
+double res = 0;
+
+string cont= "+";
+
+Queue <double> fiveLast = new Queue <double>(5);
+
+while (cont == "+")
+{
+    Console.Clear();
+    try
+    {
+        firstValue = calculator.firstValue();
+
+        secondValue = calculator.secondValue();
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Not valid action.Try again");
+        continue;
+    }
+    string repeat = calculator.chooseAction();
+
+    switch (repeat)
+    {
+        case "div":
+            Console.WriteLine($"REsult is {firstValue / secondValue}");
+            res = firstValue/ secondValue;
+            break;
+        case "add":
+            Console.WriteLine($"REsult is {firstValue + secondValue}");
+            res = firstValue + secondValue;
+            break;
+        case "sub":
+            Console.WriteLine($"REsult is {firstValue - secondValue}");
+            res = firstValue - secondValue;
+            break;
+        case "mult":
+            Console.WriteLine($"REsult is {firstValue * secondValue}");
+            res = firstValue *secondValue;
+            break;
+            default:
+            Console.WriteLine("Try again");
+            Console.ReadLine();
+            continue;
+    }
+
+    fiveLast.Enqueue(res);
+
+    if (fiveLast.Count > 5)
+    {
+        fiveLast.Dequeue();
+    }
+
+    string resultString = "";
+    foreach (var item in fiveLast)
+    {
+        resultString += item;
+    }
+
+    switch (calculator.Menu())
+    {
+        case "1":
+            cont = "+";
+            break;
+        case "2":
+            Console.WriteLine($"Five last element of the queue is {resultString}");
+            Console.ReadLine();
+            break;
+
+        case "3":
+            firstValue = calculator.firstValue();
+            secondValue = calculator.secondValue();
+            switch (repeat)
+            {
+                case "div":
+                    Console.WriteLine($"REsult is {firstValue / secondValue}");
+                    res = firstValue / secondValue;
+                    break;
+                case "add":
+                    Console.WriteLine($"REsult is {firstValue + secondValue}");
+                    res = firstValue + secondValue;
+                    break;
+                case "sub":
+                    Console.WriteLine($"REsult is {firstValue - secondValue}");
+                    res = firstValue - secondValue;
+                    break;
+                case "mult":
+                    Console.WriteLine($"REsult is {firstValue * secondValue}");
+                    res = firstValue * secondValue;
+                    break;
+                default:
+                    Console.WriteLine("Try again");
+                    break;
+            }
+            Console.ReadLine ();
+            break;
+
+        case "4":
+            cont = "-";
+            break;
+        default:
+            Console.WriteLine("Something wrong.Try again!");
+            continue;
+
+    }
+
+}
+
+
---
+++ LIbrary/Class1.cs
@@ -0,0 +1,38 @@
+﻿
+namespace LIbrary
+{
+    public class calculator
+    {
+        public static double firstValue()
+        {
+            double firstValue;
+            Console.WriteLine("Enter first value.");
+            firstValue = Convert.ToDouble(Console.ReadLine());
+            return firstValue;
+        }
+
+        public static double secondValue()
+        {
+            double secondValue;
+            Console.WriteLine("Enter second value.");
+            secondValue = Convert.ToDouble(Console.ReadLine());
+            return secondValue;
+        }
+
+        public static string chooseAction ()
+        { 
+            Console.WriteLine("Choose one of the action: div, add, sub, mult.");
+            string action = Console.ReadLine();
+            return action;
+            
+        }
+
+        public static string Menu()
+        {
+            Console.WriteLine("Choose one of the action: \n\n1.Continue claculatings? \n\t2.Output five last results? \n\t\t3.Repeat last action. \n\t\t\t 4.Exit the program.");
+            string cont = Console.ReadLine();
+            return cont;
+        }
+
+    }
+}
